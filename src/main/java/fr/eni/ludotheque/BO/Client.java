00c5fce32e01a6f12eb1072f4a86f251930b0e0d
chrysantheme15 <chrysantheme.razafindrabodo2026@campus-eni.fr>
2026-06-17 14:08:20 +0200

    MAJ bo client

--- src/main/java/fr/eni/ludotheque/BO/Client.java
+++ src/main/java/fr/eni/ludotheque/BO/Client.java
@@ -1,4 +1,30 @@
-package fr.eni.ludotheque.bo;
+package fr.eni.ludotheque.BO;
 
-public class Clienet {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int no_client;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String nom;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String prenom;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String email;
+
+    @Transient
+    private int no_telephone;
+
 }
